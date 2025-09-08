5a51f40c7eff87b594c0c52aaa4c346d912541f2
Gor Muradxanyan <gormuradxanyan@MacBook-Pro-Gor.local>
2025-09-08 23:53:10 +0400

    Adding own Exception class

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0a6d375441cd88cd57ede11021857ce484f519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919b0f2e2de8fdc566fe5617fdc5c0bc742b8a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
