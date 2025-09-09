3a2b413f6b6816605834647a4c8759b6fbfd92cd
Gor Muradxanyan <gormuradxanyan@MacBook-Pro-Gor.local>
2025-09-09 16:18:53 +0400

    feat: adding student controller

--- Presentation/Controllers/StudentController.cs
+++ Presentation/Controllers/StudentController.cs
@@ -1,6 +1,63 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Service.DTOs;
+using Service.Interfaces;
+
 namespace Learning_Management_System.Controllers;
 
-public class StudentController
+[ApiController]
+[Route("api/students")]
+public class StudentsController : ControllerBase
 {
+    private readonly ILogger<StudentsController> _logger;
+    private readonly IServiceManager  _serviceManager;
+    public StudentsController(ILogger<StudentsController> logger, IServiceManager serviceManager)
+    {
+        _logger = logger;
+        _serviceManager = serviceManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var entities = await _serviceManager.Student.GetAllAsync();
+        return Ok(entities);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var entity = await _serviceManager.Student.GetByIdAsync(id);
+        
+        if (entity is null)
+            return NotFound();
+        return Ok(entity);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] StudentForCreationDto dto)
+    {
+        var student = new StudentEntity
+        {
+            StudentId = Guid.NewGuid(),
+            StudentName = dto.Name,
+            Email = dto.Email,
+            PhoneNumber = dto.PhoneNumber
+        };
+        await _serviceManager.Student.CreateAsync(student);
+        await _serviceManager.SaveAsync();
+        return CreatedAtAction(nameof(GetById), new { id = student.StudentId }, student);
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Guid id)
+    {
+        var answer = _serviceManager.Student.DeleteAsync(id);
+        if (!answer.Result)
+            return NotFound();
+        _serviceManager.SaveAsync();
+        return NoContent();
+    }
+    
     
 }
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,22 +1,39 @@
 using Infrastructure___Persistence;
 using Microsoft.EntityFrameworkCore;
+using Service;
+using Service.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers(); // важно, если у тебя классические контроллеры
+builder.Services.AddEndpointsApiExplorer(); // для Swagger
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-   
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddScoped<IServiceManager, ServiceManager>(); 
+builder.Services.AddScoped<ICourseService, CourseService>(); 
+builder.Services.AddScoped<ILessonService, LessonService>(); 
+builder.Services.AddScoped<IStudentService, StudentService>();
+
+
 
 var app = builder.Build();
+//app.UseRouting();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();   
+
 }
 
 app.UseHttpsRedirection();
+app.MapControllers(); 
 
 
 
